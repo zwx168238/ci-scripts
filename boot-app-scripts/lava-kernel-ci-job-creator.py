9a12d9c0cca04f0d6cc4f7b69951893a90e4a30b
wuyanjun <wuyanjun@hisilicon.com>
2016-05-28 18:36:22 +0800

    support the profile of test cases, such as UT/ST and so on

--- boot-app-scripts/lava-kernel-ci-job-creator.py
+++ boot-app-scripts/lava-kernel-ci-job-creator.py
@@ -19,26 +19,6 @@ arch_distro = {'d01': ['Ubuntu_ARM32.tar.gz'],
                'd02': ['Fedora_ARM64.tar.gz', 'Ubuntu_ARM64.tar.gz',
                    'Debian_ARM64.tar.gz', 'OpenSuse_ARM64.tar.gz', 'CentOS_ARM64.tar.gz']}
 
-panda_es = {'device_type': 'panda-es',
-            'templates': ['generic-arm-dtb-kernel-ci-boot-template.json',
-                          'generic-arm-dtb-kernel-ci-kselftest-template.json',
-                          'generic-arm-dtb-kernel-ci-hackbench-template.json'],
-            'defconfig_blacklist': ['arm-allmodconfig'],
-            'kernel_blacklist': [],
-            'nfs_blacklist': [],
-            'lpae': False,
-            'fastboot': False}
-
-panda = {'device_type': 'panda',
-         'templates': ['generic-arm-dtb-kernel-ci-boot-template.json',
-                       'generic-arm-dtb-kernel-ci-kselftest-template.json',
-                       'generic-arm-dtb-kernel-ci-hackbench-template.json'],
-         'defconfig_blacklist': ['arm-allmodconfig'],
-         'kernel_blacklist': [],
-         'nfs_blacklist': [],
-         'lpae': False,
-         'fastboot': False}
-
 hisi_x5hd2_dkb = {'device_type': 'hi3716cv200',
                   'templates': ['generic-arm-dtb-kernel-ci-boot-template.json',
                                 'generic-arm-dtb-kernel-ci-boot-nfs-template.json',
@@ -63,17 +43,17 @@ d01 = {'device_type': 'd01',
        'fastboot': False}
 
 d02 = {'device_type': 'd02',
-		'templates': ['d02-arm64-kernel-ci-boot-template.json',
+    'templates': ['d02-arm64-kernel-ci-boot-template.json',
                               'd02-arm64-kernel-ci-boot-sata-template.json',
                               'd02-arm64-kernel-ci-boot-nfs-template.json',
-			      'd02-arm64-kernel-ci-weekly-template.json'],
-		'defconfig_blacklist': ['arm64-allnoconfig',
-					'arm64-allmodconfig'],
-		'kernel_blacklist': [],
-	        'nfs_blacklist': [],
-		'lpae': False,
-		'be': False,
-		'fastboot': False}
+                              'd02-arm64-kernel-ci-weekly-template.json'],
+    'defconfig_blacklist': ['arm64-allnoconfig',
+                            'arm64-allmodconfig'],
+                            'kernel_blacklist': [],
+                            'nfs_blacklist': [],
+                            'lpae': False,
+                            'be': False,
+                            'fastboot': False}
 
 hi6220_hikey = {'device_type': 'hi6220-hikey',
                 'templates': ['generic-arm64-dtb-kernel-ci-boot-template.json',
@@ -101,78 +81,14 @@ dummy_ssh = {'device_type': 'dummy_ssh',
                             'network_tests_basic.json',
                             'sysbench.json'],}
 
-snowball = {'device_type': 'snowball',
-            'templates': ['generic-arm-dtb-kernel-ci-boot-template.json',
-                          'generic-arm-dtb-kernel-ci-boot-nfs-template.json',
-                          'generic-arm-dtb-kernel-ci-boot-nfs-mp-template.json',
-                          'generic-arm-dtb-kernel-ci-ltp-mm-template.json',
-                          'generic-arm-dtb-kernel-ci-ltp-syscalls-template.json',
-                          'generic-arm-dtb-kernel-ci-kselftest-template.json',
-                          'generic-arm-dtb-kernel-ci-hackbench-template.json'],
-            'defconfig_blacklist': ['arm-allmodconfig'],
-            'kernel_blacklist': [],
-            'nfs_blacklist': [],
-            'lpae': False,
-            'fastboot': False}
-
-x86 = {'device_type': 'x86',
-       'templates': ['generic-x86-kernel-ci-boot-template.json',
-                     'generic-x86-kernel-ci-kselftest-template.json',
-                     'generic-x86-kernel-ci-hackbench-template.json'],
-       'defconfig_blacklist': ['x86-i386_defconfig',
-                               'x86-allnoconfig',
-                               'x86-allmodconfig',
-                               'x86-allmodconfig+CONFIG_OF=n',
-                               'x86-tinyconfig',
-                               'x86-kvm_guest.config'],
-       'kernel_blacklist': [],
-       'nfs_blacklist': [],
-       'lpae': False,
-       'fastboot': False}
-
-x86_atom330 = {'device_type': 'x86-atom330',
-                         'templates': ['generic-x86-kernel-ci-boot-template.json',
-                                       'generic-x86-kernel-ci-kselftest-template.json',
-                                       'generic-x86-kernel-ci-hackbench-template.json'],
-                         'defconfig_blacklist': ['x86-i386_defconfig',
-                                                 'x86-allnoconfig',
-                                                 'x86-allmodconfig',
-                                                 'x86-allmodconfig+CONFIG_OF=n',
-                                                 'x86-tinyconfig',
-                                                 'x86-kvm_guest.config'],
-                         'kernel_blacklist': [],
-                         'nfs_blacklist': [],
-                         'lpae': False,
-                         'fastboot': False}
-
-x86_kvm = {'device_type': 'kvm',
-           'templates': ['generic-x86-kernel-ci-boot-template.json',
-                         'generic-x86-kernel-ci-kselftest-template.json',
-                         'generic-x86-kernel-ci-hackbench-template.json'],
-           'defconfig_blacklist': ['x86-i386_defconfig',
-                                   'x86-allnoconfig',
-                                   'x86-allmodconfig',
-                                   'x86-allmodconfig+CONFIG_OF=n',
-                                   'x86-tinyconfig',
-                                   'x86-kvm_guest.config'],
-           'kernel_blacklist': ['v3.10',
-                                'lsk-v3.10'],
-           'nfs_blacklist': [],
-           'lpae': False,
-           'fastboot': False}
-
-device_map = {'omap4-panda-es.dtb': [panda_es],
-              'omap4-panda.dtb': [panda],
-              'hip04-d01.dtb': [d01],
+device_map = {'hip04-d01.dtb': [d01],
               'hip05-d02.dtb': [d02],
               'hisi-x5hd2-dkb.dtb': [hisi_x5hd2_dkb],
-              'ste-snowball.dtb': [snowball],
               #'qemu-arm-legacy': [qemu_arm],
               #'qemu-aarch64-legacy': [qemu_aarch64],
               #'juno.dtb': [juno, juno_kvm],
               #'hi6220-hikey.dtb': [hi6220_hikey],
-              'x86': [x86, x86_atom330],
-              'x86-kvm': [x86_kvm]}
+              }
 
 parse_re = re.compile('href="([^./"?][^"?]*)"')
 
@@ -265,11 +181,38 @@ def create_jobs(base_url, kernel, plans, platform_list, targets, priority,
                 #elif not any([x for x in defconfigs if x == defconfig]) and plan != 'boot':
                 #    print '%s has been omitted from the %s test plan. Skipping JSON creation.' % (defconfig, plan)
                 else:
-                    total_templates = [ x for x in device_templates]
+                    # add by wuyanjun in 2016/5/28
+                    # add the profile of test cases, so only UT test case can be
+                    # executed or ST can be executed.
                     for json in dummy_ssh['templates']:
-                        total_templates.append(json)
+                        device_templates.append(json)
+
+                    total_templates = []
+                    config_plan = ConfigParser.ConfigParser()
+                    config_plan.read(cwd + '/templates/' + plan + '/' + plan + '.ini')
+                    if test_kind != "BOTH":
+                        single_templates = []
+                        both_templates = []
+                        try:
+                            single_templates = [ x for x in device_templates if \
+                                    x.split(".json")[0] in \
+                                    config_plan.get("TEST_KIND", test_kind).split(",")]
+                        except:
+                            print "There is no %s test cases" % test_kind
+                        try:
+                            both_templates = [ x for x in device_templates if \
+                                    x.split(".json")[0] in \
+                                    config_plan.get("TEST_KIND", 'BOTH').split(",")]
+                        except:
+                            print "There is no UT and ST test cases"
+                        total_templates = list(set(single_templates).union(set(both_templates)))
+                    else:
+                        # may be need to improve here because of all test cases will be executed
+                        total_templates = [x for x in device_templates] 
+
                     for template in total_templates:
-                        job_name = tree + '-' + kernel_version + '-' + defconfig[:100] + '-' + platform_name + '-' + device_type + '-' + plan
+                        job_name = tree + '-' + kernel_version + '-' + defconfig[:100] + \
+                                '-' + platform_name + '-' + device_type + '-' + plan
                         if template in dummy_ssh['templates']:
                             job_json = cwd + '/jobs/' + job_name + '-' + template
                         else:
@@ -282,6 +225,8 @@ def create_jobs(base_url, kernel, plans, platform_list, targets, priority,
                                         tmp = line.replace('{dtb_url}', platform)
                                         tmp = tmp.replace('{kernel_url}', kernel)
                                         # add by wuyanjun
+                                        # if the jobs are not the boot jobs of LAVA, try to use the 
+                                        # dummy_ssh as the board device, or use the ${board_type} itself.
                                         if 'boot' not in plan and 'BOOT' not in plan:
                                             tmp = tmp.replace('{device_type}', 'dummy_ssh'+'_'+device_type)
                                         else:
@@ -293,25 +238,6 @@ def create_jobs(base_url, kernel, plans, platform_list, targets, priority,
                                         # end by wuyanjun
                                         tmp = tmp.replace('{image_type}', image_type)
                                         tmp = tmp.replace('{image_url}', image_url)
-                                        modules_url = image_url + 'modules.tar.xz'
-                                        dummy_modules_url = 'https://googledrive.com/host/0B9DbsE2BbZ7ufjdLMVVONThlbE1mR3N4TjdFTVJod2c4TXpRUDZjMmF0Ylp4Ukk5VG14Ync/images/modules/modules.tar.xz'
-                                        if has_modules:
-                                            # Check if the if the modules actually exist
-                                            if not checked_modules:
-                                                # We only need to check that the modules
-                                                # exist once for each defconfig
-                                                p = urlparse.urlparse(modules_url)
-                                                conn = httplib.HTTPConnection(p.netloc)
-                                                conn.request('HEAD', p.path)
-                                                resp = conn.getresponse()
-                                                if resp.status > 400:
-                                                    has_modules = False
-                                                    print "No modules found, using dummy modules"
-                                                    modules_url = dummy_modules_url
-                                                checked_modules = True
-                                        else:
-                                            modules_url = dummy_modules_url
-                                        tmp = tmp.replace('{modules_url}', modules_url)
                                         tmp = tmp.replace('{tree}', tree)
                                         if platform_name.endswith('.dtb'):
                                             tmp = tmp.replace('{device_tree}', platform_name)
@@ -339,6 +265,7 @@ def create_jobs(base_url, kernel, plans, platform_list, targets, priority,
                                             tmp = tmp.replace('{priority}', 'high')
                                         fout.write(tmp)
                             # add by wuyanjun 2016/3/8
+                            # to support filling all the nfsroot url in the json template
                             with open(job_json, 'rb') as temp:
                                 whole_lines = temp.read()
                             if re.findall('nfs_url', whole_lines):
@@ -346,12 +273,14 @@ def create_jobs(base_url, kernel, plans, platform_list, targets, priority,
                                 if len(distro_list):
                                     fill_nfs_url(job_json, distro_list, device_type)
                             # add by wuyanjun 2016/5/12
+                            # to support showing the distro name in the process of the SAS boot
                             if sasFlag:
                                 new_name = job_json.split(".json")[0] + '-' + distro + '.json'
                                 os.rename(job_json, new_name)
                                 job_json = new_name
                             print 'JSON Job created: jobs/%s' % job_json.split('/')[-1]
 
+# to fill the {nfs_url} instead of ${rootnfs_address_url}
 def fill_nfs_url(job_json, distro_list, device_type):
     select_distro = [ x for x in distro_list if x.split('/')[-1] in arch_distro[device_type]]
     for distro in distro_list:
@@ -367,7 +296,7 @@ def fill_nfs_url(job_json, distro_list, device_type):
                     if re.search('{nfs_distro}', tmp):
                         tmp = line.replace('{nfs_distro}', rootfs_name)
                     fout.write(tmp)
-            print 'JSON Job created: jobs/%s' % modified_file.split('/')[-1]
+            #print 'JSON Job created: jobs/%s' % modified_file.split('/')[-1]
     if os.path.exists(job_json):
         os.remove(job_json)
 
@@ -444,9 +373,11 @@ def walk_url(url, distro_url, plans=None, arch=None, targets=None,
             legacy_platform_list = []
 
     for dir in dirs:
-        walk_url(url + dir, distro_url, plans, arch, targets, priority, distro, SasFlag)
+        walk_url(url + dir, distro_url, plans, arch, targets, priority,\
+                distro, SasFlag)
 
 def main(args):
+    global test_kind
     config = configuration.get_config(args)
 
     setup_job_dir(os.getcwd() + '/jobs')
@@ -454,8 +385,12 @@ def main(args):
     distro = config.get("distro")
     if distro is None:
         distro = "Ubuntu"
-    walk_url(config.get("url"), config.get("url"), config.get("plans"), config.get("arch"),
-            config.get("targets"), config.get("priority"), distro, config.get("SasFlag"))
+    test_kind = config.get("testClassify")
+    if test_kind is None:
+        test_kind = "BOTH"
+    walk_url(config.get("url"), config.get("url"), config.get("plans"),
+            config.get("arch"), config.get("targets"), config.get("priority"),
+            distro, config.get("SasFlag"))
     print 'Done scanning for kernel information'
     print 'Done creating JSON jobs'
     exit(0)
@@ -464,16 +399,25 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("url", help="url to build artifacts")
     parser.add_argument("--config", help="configuration for the LAVA server")
-    parser.add_argument("--section", default="default", help="section in the LAVA config file")
-    parser.add_argument("--plans", nargs='+', required=True, help="test plan to create jobs for")
-    parser.add_argument("--arch", help="specific architecture to create jobs for")
-    parser.add_argument("--targets", nargs='+', help="specific targets to create jobs for")
-    parser.add_argument("--priority", choices=['high', 'medium', 'low', 'HIGH', 'MEDIUM', 'LOW'],
+    parser.add_argument("--section", default="default", help="section in the\
+            LAVA config file")
+    parser.add_argument("--plans", nargs='+', required=True, help="test plan\
+            to create jobs for")
+    parser.add_argument("--arch", help="specific the architecture to create jobs\
+            for")
+    parser.add_argument("--targets", nargs='+', help="specific targets to create\
+            jobs for")
+    parser.add_argument("--priority", choices=['high', 'medium', 'low', 'HIGH',\
+            'MEDIUM', 'LOW'],
                         help="priority for LAVA jobs")
-    parser.add_argument("--distro", choices=['Ubuntu', 'OpenSuse', 'Debian', 'Fedora', 'CentOS'],
+    parser.add_argument("--distro", choices=['Ubuntu', 'OpenSuse', 'Debian', \
+            'Fedora', 'CentOS'],
                         help="distro for sata deploying")
     # the SasFlag is used to flag if the lava job will use the Distro in the job name
     # when there is not {nfs_url} in the job definition
     parser.add_argument('--SasFlag', action='store_true')
+    # BOTH means the case are both UT and ST
+    parser.add_argument('--testClassify', help="the argument to distinguish \
+            which tests run", choices=['UT', "ST", "BOTH"])
     args = vars(parser.parse_args())
     main(args)

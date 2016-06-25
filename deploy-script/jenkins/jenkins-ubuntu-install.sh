1dc0b985b4959b837bb14f5e724cab88080cb623
Wei Xu <xuwei5@hisilicon.com>
2016-06-26 06:49:32 +0800

    deploy: update the jenins depoy script to use the jenksin job builder to import jobs
    Signed-off-by: Wei Xu <xuwei5@hisilicon.com>

--- deploy-script/jenkins/jenkins-ubuntu-install.sh
+++ deploy-script/jenkins/jenkins-ubuntu-install.sh
@@ -18,7 +18,30 @@ sudo service jenkins restart
 echo "sleep 90 seconds to wait jenkins restart"
 sleep 90
 
-#import the job for the jenkins
-java -jar jenkins-cli.jar -s http://localhost:8082/ create-job trigger-flex < hisilicon-script/trigger-flex.xml
-java -jar jenkins-cli.jar -s http://localhost:8082/ create-job khilman-kbuilder < hisilicon-script/khilman-kbuilder.xml
-java -jar jenkins-cli.jar -s http://localhost:8082/ create-job khilman-kernel-build-complete < hisilicon-script/khilman-kernel-build-complete.xml
+#install jenkins job builder
+sudo apt-get install -y python-pip
+sudo pip install jenkins-job-builder
+
+#import the jobs
+git clone -b openlab2 https://github.com/open-estuary/ci-scripts.git
+cd ci-scripts/jenkins-job-config/
+jenkins-jobs --conf etc/jenkins_jobs.ini update  khilman-trigger-flex.yaml
+jenkins-jobs --conf etc/jenkins_jobs.ini update  khilman-kbuilder.yaml
+jenkins-jobs --conf etc/jenkins_jobs.ini update  khilmna-kernel-build-complete.yaml
+jenkins-jobs --conf etc/jenkins_jobs.ini update  kernelci-kboot-bot.yaml
+
+#create /var/www/images/kernerl-ci 
+sudo mkdir -p /var/www/images/kernel-ci
+sudo chmod -R 777 /var/www/images/kernel-ci
+sudo chown -R www-data /var/www/images/kernel-ci
+
+#create /srv/mirrors/linux.git
+sudo mkdir -p /srv/mirrors
+sudo git clone https://github.com/hisilicon/linux-hisi.git /srv/mirrors/linux.git
+
+#file server
+sudo apt-get install nginx
+sudo cp kernelci-fileserver /etc/nginx/sites-available
+sudo ln -s /etc/nginx/sites-available/kernelci-fileserver /etc/nginx/sites-enabled/kernelci-fileserver
+sudo rm /etc/nginx/sites-enabled/default
+sudo service nginx restart

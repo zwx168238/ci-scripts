7e05d85e04dd803c0342b663f7b21a51bca403b5
wuyanjun <wuyanjun1027@163.com>
2016-07-03 09:36:26 +0800

    fix code

--- boot-app-scripts/modify_conf_file.sh
+++ boot-app-scripts/modify_conf_file.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 IFS=$'\n'
 
 ip_addr_file="device_ip_type.txt"
@@ -12,18 +11,17 @@ do
     while read line
     do 
         valid=$(echo $line | grep -w $target_instance)
-	if [ ""x != "$valid"x ]; then
-	    ip_addr=$(echo $line | awk '{print $3}')
-            target_device=$(echo $line | awk '{print $2}')
+	    if [ ""x != "$valid"x ]; then
+	        ip_addr=$(echo $line | awk '{print $3}')
             flag=1
-	    break
-	fi
+	        break
+	    fi
     done < $ip_addr_file
     if [ $flag -eq 1 ]; then
-	device_conf=$target_device'_ssh.conf'
-	cp $device_conf  $device_conf".1"
-        echo $ip_addr   '  '  $device_conf
-	sed -i "s/^dummy_ssh_host.*/dummy_ssh_host = $ip_addr/g"  $device_conf
+	    device_conf=$target_instance'_ssh.conf'
+	    #cp $device_conf  $device_conf".1"
+        echo $ip_addr   '  '  $device_conf ' target_instance:' $target_instance
+	    sed -i "s/^dummy_ssh_host.*/dummy_ssh_host = $ip_addr/g"  $device_conf
     fi
 done
 

27419803ef5a0824fba2a818ec2d06fed997aac6
秦书磊 <qinsl0106@thundersoft.com>
2017-04-26 19:25:33 +0800

    add version

--- docker/start-jenkins.sh
+++ docker/start-jenkins.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ${WORK_HOME}
-docker run -d -p 2002:8080 -p 2003:50000 --name myjenkins -v ./jenkins_home/:/var/jenkins_home njdocker1.nj.thundersoft.com/public/jenkins
+docker run -d -p 2002:8080 -p 2003:50000 --name myjenkins -v ./jenkins_home/:/var/jenkins_home njdocker1.nj.thundersoft.com/public/jenkins:2.32.3

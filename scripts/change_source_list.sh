fda967eac5376b034a16be5878bb7529b6c0e422
秦书磊 <qinsl0106@thundersoft.com>
2017-04-26 10:30:36 +0800

    change source list

---
+++ scripts/change_source_list.sh
@@ -0,0 +1,18 @@
+ #!/bin/bash -e
+
+sudo mv /etc/apt/source.list /etc/apt/source.list.bak | true
+
+sudo cat > /etc/apt/source.list <<EOF
+deb http://mirrors.163.com/ubuntu/ trusty main restricted universe multiverse
+deb http://mirrors.163.com/ubuntu/ trusty-security main restricted universe multiverse
+deb http://mirrors.163.com/ubuntu/ trusty-updates main restricted universe multiverse
+deb http://mirrors.163.com/ubuntu/ trusty-proposed main restricted universe multiverse
+deb http://mirrors.163.com/ubuntu/ trusty-backports main restricted universe multiverse
+deb-src http://mirrors.163.com/ubuntu/ trusty main restricted universe multiverse
+deb-src http://mirrors.163.com/ubuntu/ trusty-security main restricted universe multiverse
+deb-src http://mirrors.163.com/ubuntu/ trusty-updates main restricted universe multiverse
+deb-src http://mirrors.163.com/ubuntu/ trusty-proposed main restricted universe multiverse
+deb-src http://mirrors.163.com/ubuntu/ trusty-backports main restricted universe multiverse
+EOF
+
+sudo apt-get update

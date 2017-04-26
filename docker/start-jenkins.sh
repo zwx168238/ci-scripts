2fc735b7f0c22760d4f5fe94c59cdea3887ab507
秦书磊 <qinsl0106@thundersoft.com>
2017-04-26 19:30:08 +0800

    ensure permission

--- docker/start-jenkins.sh
+++ docker/start-jenkins.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd ${WORK_HOME}
+mkdir -p ${WORK_HOME}/jenkins_home
+chmod a+r ${WORK_HOME}/jenkins_home
 docker run -d -p 2002:8080 -p 2003:50000 --name myjenkins -v ${WORK_HOME}/jenkins_home/:/var/jenkins_home njdocker1.nj.thundersoft.com/public/jenkins:2.32.3

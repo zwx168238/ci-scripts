f69ef15a0f3b98423482b479987048b5789666c7
秦书磊 <qinsl0106@thundersoft.com>
2017-04-26 12:08:37 +0800

    add trap and fix logic

--- scripts/update_estuary_reference.sh
+++ scripts/update_estuary_reference.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -ex
+trap "exit 1" SIGINT SIGTERM
 
 ###################### prepare repo tool ######################
 if [ ! -e bin ]; then
@@ -10,13 +11,17 @@ fi
 export PATH=${WORKSPACE}/bin:$PATH;
 
 # sync
+mkdir -p ~/estuary_reference
 cd ~/estuary_reference
 
-if [ -d '.repo' ];then
+if [ ! -d '.repo' ];then
     repo init -u "https://github.com/open-estuary/estuary.git" --mirror --no-repo-verify --repo-url=git://android.git.linaro.org/tools/repo
 fi
 
+set +e
 false; while [ $? -ne 0 ]; do repo sync --force-sync; done
+set -e
+
 repo status
 
 print_time "the end time of finishing downloading estuary is "

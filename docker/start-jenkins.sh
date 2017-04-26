a7fbfcb8440d0bd4ec7bc06cd076e49c32f948b6
秦书磊 <qinsl0106@thundersoft.com>
2017-04-26 19:31:19 +0800

    auto remove the old myjenkins

--- docker/start-jenkins.sh
+++ docker/start-jenkins.sh
@@ -2,4 +2,4 @@
 cd ${WORK_HOME}
 mkdir -p ${WORK_HOME}/jenkins_home
 chmod a+r ${WORK_HOME}/jenkins_home
-docker run -d -p 2002:8080 -p 2003:50000 --name myjenkins -v ${WORK_HOME}/jenkins_home/:/var/jenkins_home njdocker1.nj.thundersoft.com/public/jenkins:2.32.3
+docker run --rm -d -p 2002:8080 -p 2003:50000 --name myjenkins -v ${WORK_HOME}/jenkins_home/:/var/jenkins_home njdocker1.nj.thundersoft.com/public/jenkins:2.32.3

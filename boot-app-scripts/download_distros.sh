a636427222d00f5df722df25cb1c6aeb4ddeb3c2
秦书磊 <qinsl0106@thundersoft.com>
2017-04-27 14:55:34 +0800

    add more info

--- boot-app-scripts/download_distros.sh
+++ boot-app-scripts/download_distros.sh
@@ -1,27 +1,26 @@
-#!/bin/bash
-download_distro()
-{
+#!/bin/bash -ex
+function download_distro() {
     Distro=$1
     download_url=$2
     arch=$3
     board=$4
     distro=$(echo $distro | tr '[:upper:]' '[:lower:]')
     ARCH=$(echo $arch | tr '[:lower:]' '[:upper:]')
-	BOARD=$(echo $board | tr '[:lower:]' '[:upper:]')
+    BOARD=$(echo $board | tr '[:lower:]' '[:upper:]')
     distro_def=${Distro}_$ARCH
-	echo $distro_def
+    echo $distro_def
     pushd $location
-	pushd $distro_loc
-	    [ ! -d $BOARD ] && mkdir $BOARD
-	    pushd $BOARD
-	    dist_name=${distro}$ARCH
-	    if [ -d $dist_name ]; then
-		sudo rm -rf $dist_name
-	    fi
-	    sudo mkdir $dist_name
-	    wget -P $dist_name -c $download_url/distro/${distro_def}.tar.gz
-	    popd
-	popd
+    pushd $distro_loc
+    [ ! -d $BOARD ] && mkdir $BOARD
+    pushd $BOARD
+    dist_name=${distro}$ARCH
+    if [ -d $dist_name ]; then
+        sudo rm -rf $dist_name
+    fi
+    sudo mkdir $dist_name
+    wget -P $dist_name -c $download_url/distro/${distro_def}.tar.gz
+    popd
+    popd
     popd
 }
 
@@ -32,4 +31,4 @@ download_url=$2
 arch=$3
 board=$4
 echo "Download $distro from $dowload_url"
-download_distro $distro  $download_url $arch $board
+download_distro $distro $download_url $arch $board

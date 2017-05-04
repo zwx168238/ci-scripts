4ed02a11419a9e8240ce4314b7f3ed7deb5ca837
秦书磊 <qinsl0106@thundersoft.com>
2017-05-04 13:18:58 +0800

    add mount command

--- scripts/start-lava-service.sh
+++ scripts/start-lava-service.sh
@@ -7,3 +7,6 @@ services=(ssh postgresql tftpd-hpa apache2 lava-slave lava-coordinator lava-mast
 for i in ${services[@]};do
     service $i start
 done
+
+# mount NFS
+mount -t nfs 192.168.3.100:/targetNFS/ /mount/NFS
